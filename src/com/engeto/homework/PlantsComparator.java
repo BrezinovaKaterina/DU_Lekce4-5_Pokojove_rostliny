545b4c0aec6ad65335e837570aa5e0143e939f1e
BrezinovaKaterina <144615454+BrezinovaKaterina@users.noreply.github.com>
2023-11-02 18:04:14 +0100

    Lekce 5
    Porovnání dle data poslední zálivky (Comparator)

---
+++ src/com/engeto/homework/PlantsComparator.java
@@ -0,0 +1,12 @@
+package com.engeto.homework;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+
+public class PlantsComparator implements Comparator<Plant> {
+
+    @Override
+    public int compare(Plant o1, Plant o2) {
+        return o1.getWatering().compareTo(o2.getWatering());
+    }
+}
